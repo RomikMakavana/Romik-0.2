7269c0610f58b8684a4d65c6d83da3a44ed51082
RomikMakavana <40670677+RomikMakavana@users.noreply.github.com>
2018-08-26 17:51:28 +0530

    Update header.php

--- backend/pages/layout/header.php
+++ backend/pages/layout/header.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand gedeant">Romik<sub>0.1</sub></a>
+    <a class="navbar-brand gedeant">Romik<sub>0.2</sub></a>
     <div>
         <a href="<?= \libs\Romik::toAdminUrl('user/changePassword') ?>"><button class="btn btn-outline-primary" type="submit">Change-Password</button></a>
         <a href="<?= \libs\Romik::toAdminUrl('user/logout') ?>"><button class="btn btn-outline-danger" type="submit">Logout</button></a>
